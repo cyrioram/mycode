5db77e1f65c99965e3a299ca749adc69d85fc55a
David Oram <coram007@gmail.com>
2020-09-21 17:10:50 +0000

    2nd commit

---
+++ no_shebang.py
@@ -0,0 +1,8 @@
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+
+
+# function is a repeatable blockl of code
+
+# method only callable by a certain class
